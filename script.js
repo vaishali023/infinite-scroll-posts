f5d83ace85a483431715580d316aa61b548494e5
vaishali023 <vaishalichaudhary011@gmail.com>
2022-05-06 13:06:36 -0400

    filter posts

--- script.js
+++ script.js
@@ -33,7 +33,48 @@ async function showPosts() {
       postsContainer.appendChild(postEl);
     });
   }
-  
 
-//Show initial posts
+  //Show loader and fetch more posts
+  function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+      loading.classList.remove('show');
+
+      setTimeout(() => {
+        page++;
+        showPosts();
+      }, 300);
+    }, 1000);
+  }
+
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
+// Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
